dab291e1d427ecfe1e14fdfbba1286c12bdc9758
Alessandro Baldassarre <113554225+aleabalda@users.noreply.github.com>
2023-11-17 19:51:05 -0700

    Update Member.java

--- src/main/java/model/role/Member.java
+++ src/main/java/model/role/Member.java
@@ -1,28 +1,16 @@
 package main.java.model.role;
 
+import main.java.utils.Address;
+
 public class Member extends User {
     private String prefEmail;
     private String name;
-    private int addrCode;
-    private String addrStreet;
-    private String addrDirection; // Northwest, Southeast, etc.
+    private Address address;
 
-    /*
-     * I feel like we should put all the address information into its own object
-     * kind of how he discussed in class
-     * because an address would be separated into multiple pieces of information not
-     * just the ones I listed above
-     * (postal code, city, province/state) I'm just not sure where we would put an
-     * "Address" class. The code will look cleaner.
-     */
-
-    public Member(String username, String password, String name, int addrCode, String addrStreet,
-            String addrDirection) {
+    public Member(String username, String password, String name, Address address) {
         super(username, password);
         this.name = name;
-        this.addrCode = addrCode;
-        this.addrDirection = addrDirection;
-        this.addrStreet = addrStreet;
+        this.address = address;
 
         // Add code in here so when a user registers and member object is created, the
         // information is stored in the database right away.
@@ -37,16 +25,12 @@ public class Member extends User {
         return name;
     }
 
-    public int getAddrCode() {
-        return addrCode;
+    public Address getAddress() {
+        return address;
     }
 
-    public String getAddrStreet() {
-        return addrStreet;
-    }
-
-    public String getAddrDirection() {
-        return addrDirection;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     public void setMemberEmail(String newEmail) {
@@ -57,15 +41,4 @@ public class Member extends User {
         this.name = newName;
     }
 
-    public void setAddrCode(int newCode) {
-        this.addrCode = newCode;
-    }
-
-    public void setAddrStreet(String newStreet) {
-        this.addrStreet = newStreet;
-    }
-
-    public void setAddrDirection(String newDirection) {
-        this.addrDirection = newDirection;
-    }
 }
