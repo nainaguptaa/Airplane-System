d7905143958ab098db368b797aea7f79f8338a18
aleabalda <alessandro.baldassar@ucalgary.ca>
2023-11-19 17:13:33 -0700

    edited member and moved address class to util

--- src/main/java/model/role/Member.java
+++ src/main/java/model/role/Member.java
@@ -3,34 +3,22 @@ package main.java.model.role;
 import java.util.Date;
 import main.java.model.util.CreditCard;
 
-import main.java.utils.Address;
+import main.java.model.util.Address;
 
 public class Member extends User {
     private String name;
     private Address address;
+    private CreditCard card;
 
-    public Member(String username, String password, String name, Address address) {
-        super(username, password);
-        this.name = name;
-        this.address = address;
-
-        // Add code in here so when a user registers and member object is created, the
-        // information is stored in the database right away.
-    }
-
-    public Member(String username, String password, String name, String email, int addrCode, String addrStreet,
-            String addrDirection, CreditCard card) {
+    public Member(String username, String password, String email, String name, Address address, CreditCard card) {
         super(username, password, email);
         this.name = name;
-        this.addrCode = addrCode;
-        this.addrDirection = addrDirection;
-        this.addrStreet = addrStreet;
+        this.address = address;
         this.card = card;
 
         // Add code in here so when a user registers and member object is created, the
         // information is stored in the database right away.
     }
-
     // getters and setters
 
     public String getMemberName() {
@@ -47,6 +35,14 @@ public class Member extends User {
 
     public void setMemberName(String newName) {
         this.name = newName;
+    }
+
+    public CreditCard getCreditCard() {
+        return card;
+    }
+
+    public void setCreditCard(CreditCard card) {
+        this.card = card;
     }
 
 }
---
+++ src/main/java/model/util/Address.java
@@ -0,0 +1,100 @@
+package main.java.model.util;
+
+enum Direction {
+    NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case NORTHWEST:
+                return "Northwest";
+            case NORTHEAST:
+                return "Northeast";
+            case SOUTHWEST:
+                return "Southwest";
+            case SOUTHEAST:
+                return "Southeast";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+}
+
+public class Address {
+    private String street;
+    private String city;
+    private String division;
+    private int code;
+    private Direction direction;
+    private String postalCode;
+    private String country;
+
+    // Constructor
+    public Address(int code, String street, Direction direction, String city, String division, String postalCode,
+            String country) {
+        this.street = street;
+        this.city = city;
+        this.division = division;
+        this.code = code;
+        this.direction = direction;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    // Getters and Setters
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public String getdivision() {
+        return division;
+    }
+
+    public void setdivision(String division) {
+        this.division = division;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
