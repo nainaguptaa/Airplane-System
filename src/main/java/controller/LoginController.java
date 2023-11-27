1dcc8bfe3ddb46836a1709332068643a30f3bc16
AarshShah9 <aarshshah99@gmail.com>
2023-11-26 17:23:56 -0700

    Close

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -5,7 +5,7 @@ import view.LoginView;
 import java.awt.event.*;
 
 import javax.naming.spi.DirStateFactory.Result;
-import javax.swing.Action;
+import javax.swing.*;
 import java.sql.ResultSet;
 
 //Will need to add listener when login view is created
@@ -65,6 +65,7 @@ public class LoginController implements ActionListener {
             // this.uc = new UserController(db);
             // this.uc.setUser(model);
         } else {
+            JOptionPane.showMessageDialog(null, "Invalid username or password");
             System.out.println("Invalid username or password");
         }
     }
