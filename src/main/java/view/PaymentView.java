5c972cd3747ba61a974474aba567922b529fcd90
nainaguptaa <naina.gupta@ucalgary.ca>
2023-11-28 04:35:45 -0700

    done

--- src/main/java/view/PaymentView.java
+++ src/main/java/view/PaymentView.java
@@ -1,180 +1,3 @@
-// package main.java.view;
-
-// import javax.swing.*;
-// import java.awt.*;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
-
-// public class PaymentView extends JFrame {
-
-//     private JRadioButton yesRadioButton;
-//     private JRadioButton noRadioButton;
-//     public PaymentView() {
-//         setTitle("Payment Page");
-//         setSize(1200, 800);
-//         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-//         JPanel panel = new JPanel(new GridLayout(3, 2, 10, 10));
-//         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-
-
-//         JLabel question = new JLabel("Would you like to add cancellation insurance ($50)?");
-//         question.setHorizontalAlignment(SwingConstants.CENTER);
-//         yesRadioButton = new JRadioButton("Yes");
-//         noRadioButton = new JRadioButton("No");
-
-//         ButtonGroup group = new ButtonGroup();
-//         group.add(yesRadioButton);
-//         group.add(noRadioButton);
-
-//         JLabel questionLabel = new JLabel("Please select a payment method.");
-//         questionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-//         JButton MasterCardButton = new JButton("MasterCard");
-//         MasterCardButton.addActionListener(new ActionListener() {
-//             @Override
-//             public void actionPerformed(ActionEvent e) {
-//                 showMasterCardDialog();
-                
-//             }
-//         });
-
-//         JButton VisaButton = new JButton("Visa");
-//         VisaButton.addActionListener(new ActionListener() {
-//             @Override
-//             public void actionPerformed(ActionEvent e) {
-//               showVisaDialog();
-//             }
-//         });
-
-//         panel.add(question);
-//         panel.add(yesRadioButton);
-//         panel.add(noRadioButton);
-//         panel.add(questionLabel);
-//         panel.add(new JLabel()); 
-//         panel.add(MasterCardButton);
-//         panel.add(new JLabel()); 
-//         panel.add(VisaButton);
-
-//         add(panel);
-//     }
-
-//     private void showMasterCardDialog() {
-//         JTextField cardNumberField = new JTextField();
-//         JTextField expiryField = new JTextField();
-//         JTextField cvvField = new JTextField();
-//         JTextField firstName = new JTextField();
-//         JTextField lastName = new JTextField();
-
-//         Object[] message = {
-//                 "First Name:", firstName,
-//                 "Last Name:", lastName,
-//                 "Card Number:", cardNumberField,
-//                 "Expiry Date:", expiryField,
-//                 "CVV:", cvvField
-//         };
-
-//         int option = JOptionPane.showOptionDialog(
-//                 this,
-//                 message,
-//                 "Enter  MasterCard Information",
-//                 JOptionPane.OK_CANCEL_OPTION,
-//                 JOptionPane.PLAIN_MESSAGE,
-//                 null,
-//                 new Object[]{"Confirm Payment", "Cancel Payment"},
-//                 "Confirm Payment"
-//         );
-
-//         if (option == JOptionPane.OK_OPTION) {
-//             String cardNumber = cardNumberField.getText();
-//             String expiryDate = expiryField.getText();
-//             String cvv = cvvField.getText();
-//             processMasterCardPayment(cardNumber, expiryDate, cvv);
-//         } else {
-//             System.out.println("Payment canceled.");
-//         }
-//     }
-
-//     private void processMasterCardPayment(String cardNumber, String expiryDate, String cvv) {
-//         // Implement credit card payment processing logic
-//         System.out.println("Processing Master Card payment...");
-//         System.out.println("Card Number: " + cardNumber);
-//         System.out.println("Expiry Date: " + expiryDate);
-//         System.out.println("CVV: " + cvv);
-//     }
-
-
-
-
-
-
-
-//     private void showVisaDialog() {
-//       JTextField cardNumberField = new JTextField();
-//       JTextField expiryField = new JTextField();
-//       JTextField cvvField = new JTextField();
-//       JTextField firstName = new JTextField();
-//       JTextField lastName = new JTextField();
-
-//       Object[] message = {
-//               "First Name:", firstName,
-//               "Last Name:", lastName,
-//               "Card Number:", cardNumberField,
-//               "Expiry Date:", expiryField,
-//               "CVV:", cvvField
-//       };
-
-//       int option = JOptionPane.showOptionDialog(
-//               this,
-//               message,
-//               "Enter Visa Card Information",
-//               JOptionPane.OK_CANCEL_OPTION,
-//               JOptionPane.PLAIN_MESSAGE,
-//               null,
-//               new Object[]{"Confirm Payment", "Cancel Payment"},
-//               "Confirm Payment"
-//       );
-
-//       if (option == JOptionPane.OK_OPTION) {
-//           String cardNumber = cardNumberField.getText();
-//           String expiryDate = expiryField.getText();
-//           String cvv = cvvField.getText();
-//           processVisaPayment(cardNumber, expiryDate, cvv);
-//       } else {
-//           System.out.println("Payment canceled.");
-//       }
-//   }
-
-//   private void processVisaPayment(String cardNumber, String expiryDate, String cvv) {
-//       // Implement credit card payment processing logic
-//       System.out.println("Processing Visa Card payment...");
-//       System.out.println("Card Number: " + cardNumber);
-//       System.out.println("Expiry Date: " + expiryDate);
-//       System.out.println("CVV: " + cvv);
-//   }
-
-//   public boolean hasCancellationInsurance() {
-//     return yesRadioButton.isSelected();
-// }
-
-//     public static void main(String[] args) {
-//         SwingUtilities.invokeLater(() -> new PaymentView().setVisible(true));
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-//good stuff
-
 package view;
 
 import viewModel.PaymentViewModel;
@@ -183,7 +6,6 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 
 
 
@@ -222,7 +44,7 @@ public class PaymentView extends JPanel {
                 PaymentViewModel pvmArr[] = {pvm};
                 paymentViewModel = pvmArr;
                 initializeGUI();
-                // Add more instances if needed
+             
         
      
     }
@@ -233,13 +55,13 @@ public class PaymentView extends JPanel {
 
         breakdownLabel = new JLabel("Price Breakdown:");
         breakdownLabel.setBounds(50, 200, 150, 20);
-        breakdownLabel.setVisible(false); // Initially set it to invisible
+        breakdownLabel.setVisible(false); 
         add(breakdownLabel);
 
         breakdownTextArea = new JTextArea();
         breakdownTextArea.setEditable(false);
         breakdownTextArea.setBounds(50, 220, 500, 150);
-        breakdownTextArea.setVisible(false); // Initially set it to invisible
+        breakdownTextArea.setVisible(false); 
         add(breakdownTextArea);
 
         cardInfoLabel = new JLabel("Please enter Credit card information:");
@@ -269,6 +91,8 @@ public class PaymentView extends JPanel {
         cvv = new JTextField();
         cvv.setBounds(160, 490, 200, 20);
         add(cvv);
+        
+
         
         confirmButton = new JButton("Confirm Payment");
         confirmButton.setBounds(480, 500, 200, 50);
@@ -310,7 +134,7 @@ public class PaymentView extends JPanel {
         PaymentTable = new JTable(tableModel);
 
         JScrollPane scrollPane = new JScrollPane(PaymentTable);
-        scrollPane.setBounds(50, 50, 500, 50);  // Adjust the bounds as needed
+        scrollPane.setBounds(50, 50, 500, 50);  
         add(scrollPane);
 
         questionLabel = new JLabel("Would you like to add cancellation insurance ($50) ?");
@@ -330,7 +154,7 @@ public class PaymentView extends JPanel {
         });
         add(yesButton);
 
-        // Add "No" button
+       
         noButton = new JButton("No");
         noButton.setBounds(140, 150, 80, 30);
         noButton.addActionListener(new ActionListener() {
@@ -372,10 +196,12 @@ public class PaymentView extends JPanel {
                 breakdownTextArea.setVisible(true);
     }
     
+    // Checks if all three fields are filled
     private boolean validateCardInfo() {
-        // Check if all three fields are filled
         return !cardNumber.getText().isEmpty() && !expirationDate.getText().isEmpty() && !cvv.getText().isEmpty();
     }
+
+
     
     private void loadPaymentInfo() {
         for (PaymentViewModel viewModel : paymentViewModel) {
@@ -387,7 +213,7 @@ public class PaymentView extends JPanel {
 
 
 
-///good stuff
+
 
 
 
